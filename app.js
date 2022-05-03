a811e3d8608a03646af47660090465ca5282cc1f
stay0nly <derkach.dmt@gmail.com>
2022-05-03 10:54:14 +0800

    Easy functionality done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById('sum-el');
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -11,3 +12,18 @@ document.getElementById("num2-el").textContent = num2
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
+function add() {
+    sumEl.textContent = "Sum: " + (num1 + num2);
+}
+
+function subtract() {
+    sumEl.textContent = "Sum: " + (num1 - num2);
+}
+
+function divide() {
+    sumEl.textContent = "Sum: " + (num1 / num2);
+}
+
+function multiply() {
+    sumEl.textContent = "Sum: " + (num1 * num2);
+}
